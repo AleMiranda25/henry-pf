16b2aff65a58859306f20d4fe77a48c7bd439628
Ale Miranda <alemiranda2511@gmail.com>
2023-11-14 13:27:23 -0300

    Prueba de Landing

--- MechServ-Client/src/App.jsx
+++ MechServ-Client/src/App.jsx
@@ -1,7 +1,18 @@
+//Functionality
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./Views/LandingPage";
+// Styles
 import "./App.css";
+// Components
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" Component={LandingPage} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
