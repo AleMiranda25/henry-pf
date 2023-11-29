2a48861619fda5a58f8e757b5baf32362bf3012b
Ale Miranda <alemiranda2511@gmail.com>
2023-11-29 19:23:51 -0300

    Fix in login

--- MechServ-Client/src/Components/LoginButton/index.jsx
+++ MechServ-Client/src/Components/LoginButton/index.jsx
@@ -3,12 +3,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 //Componentes
 
 const LoginButton = () => {
-  const { loginWithPopup } = useAuth0();
+  const { loginWithPopup, loginWithRedirect } = useAuth0();
   return (
     <>
       <button
         className="font-[Oswald] hover:brightness-110 hover:animate-pulse font-bold py-2 px-4 rounded-full bg-[#5770F4] shadow-lg shadow-[#5770F4]/50 text-white"
-        onClick={loginWithPopup}
+        onClick={loginWithRedirect}
       >
         Ingresar
       </button>
