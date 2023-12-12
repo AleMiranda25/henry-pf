b79303d9665fcb166a80227e8692bba843bcd758
Ale Miranda <alemiranda2511@gmail.com>
2023-12-12 15:12:23 -0300

    mpIntegration

--- MechServ-Client/src/Views/Detail/Detail.jsx
+++ MechServ-Client/src/Views/Detail/Detail.jsx
@@ -8,22 +8,22 @@ import CalendarCarousel from "../../Components/CalendarCarousel/CalendarCarousel
 import VehiculeList from "../../Components/VehiculeList/VehiculeList";
 
 const Detail = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch()
-    const [ turnoSeleccionado, setTurnoSeleccionado ] = useState({
-        idUser : "",
-        idVehiculo : "",
-        idServicio : "",
-        idTurno : "",
-    });
-    const services = useSelector((state) => state.serviceId);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [turnoSeleccionado, setTurnoSeleccionado] = useState({
+    idUser: "",
+    idVehiculo: "",
+    idServicio: "",
+    idTurno: "",
+  });
+  const services = useSelector((state) => state.serviceId);
 
-    useEffect(() => {
-        dispatch(getService(id));
-        setTurnoSeleccionado({... turnoSeleccionado, idServicio : id })
-    }, [])
+  useEffect(() => {
+    dispatch(getService(id));
+    setTurnoSeleccionado({ ...turnoSeleccionado, idServicio: id });
+  }, []);
 
-    console.log("turnoSeleccionado detail:", turnoSeleccionado)
+  console.log("turnoSeleccionado detail:", turnoSeleccionado);
 
   return (
     <div
@@ -32,27 +32,35 @@ const Detail = () => {
         backgroundImage: `url(${bgHome})`,
       }}
     >
-        <Navbar/>
-        <div className="flex">
-            <div>
-        <div className="flex flex-col h-2/3 w-1/2 rounded-3xl p-10 justify-evenly text-center text-[30px] backdrop-blur-sm font-bold bg-[#202123] bg-opacity-80 text-[whitesmoke] font-[Oswald]">
-              {services && <h1>{services.name.toUpperCase()}</h1>}
+      <Navbar />
+      <div className="flex">
+        <div>
+          <div className="flex flex-col h-auto w-4/5 sm:md:lg:w-1/2 rounded-3xl p-10 justify-evenly gap-3 sm:md:lg:gap-10 text-center text-lg sm:md:lg:text-[30px] backdrop-blur-sm font-bold bg-[#202123] bg-opacity-80 text-[whitesmoke] ">
+            {services && <h1>{services.name.toUpperCase()}</h1>}
             {services && <h1>Precio: ${services.price}</h1>}
-            {services && (<p className=" text-2xl text-justify">{services.description}</p>)}
+            {services && (
+              <p className=" text-base text-justify">{services.description}</p>
+            )}
+          </div>
         </div>
-            </div>
-            <div>
-                <div className="h-2/3 w-1/3" >
-                <CalendarCarousel turnos={services ? services.turnos : undefined} turnoSeleccionado={turnoSeleccionado} setTurnoSeleccionado={setTurnoSeleccionado}/>
-                </div>
-            </div>
-            <div>
-                <div className="h-2/3 w-1/3" >
-                <VehiculeList turnoSeleccionado={turnoSeleccionado} setTurnoSeleccionado={setTurnoSeleccionado}/>
-                </div>
-            </div>
+        <div>
+          <div className="h-2/3 w-1/3">
+            <CalendarCarousel
+              turnos={services ? services.turnos : undefined}
+              turnoSeleccionado={turnoSeleccionado}
+              setTurnoSeleccionado={setTurnoSeleccionado}
+            />
+          </div>
         </div>
-
+        <div>
+          <div className="h-2/3 w-1/3">
+            <VehiculeList
+              turnoSeleccionado={turnoSeleccionado}
+              setTurnoSeleccionado={setTurnoSeleccionado}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
